53a16e748caeaeebdf39bd5510d33e5d4961892f
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-15 17:36:08 -0300

    Atualização

--- script.php
+++ script.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div>
+
+  <section class="principal-resposta">
+  <div class="resultado">
         <?php 
             $favcolor = $_POST["favcolor"];
             switch ($favcolor) {
@@ -24,6 +27,10 @@
              }   
         
         ?>
+
+        <a href="index.html" id="voltar">Retornar</a>
     </div>
+  </section>
+
 </body>
 </html>
